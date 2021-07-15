d08a2b946f89cb7deaddbf843ee053f318f25815
Damian <justice.damian96@gmail.com>
2021-07-15 15:04:40 +1000

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+let Calculator = {add: function(a, b) {return a + b;}, subtract: function(){return 1 - 3;}, multiply: function(){return 1 * 3;}, divide: function() { return 10 / 5;}};
+
+
+function actionApplyer(startInt = 0, arrFunc) {
+    let test = startInt;
+    for (let i = 0; i < (arrFunc.length); i++) {
+        test = arrFunc[i](test);
+    }
+    return test;
+}
+
+
